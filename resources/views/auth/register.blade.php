75f1812bfbdda97e7e6b3a53a4bc36a3978cb955
Christine Stone <cc@battab.com>
2017-09-27 22:31:02 -0600

    remove register ability

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Register</div>
                 <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
+                   
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
@@ -67,7 +67,7 @@
                                 </button>
                             </div>
                         </div>
-                    </form>
+     
                 </div>
             </div>
         </div>
