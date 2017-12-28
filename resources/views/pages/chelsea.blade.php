db0bc1efcb8882f9f302849cc1e900eadca2d661
Christine Stone <cc@battab.com>
2017-12-28 15:37:27 +0900

    add formatting for chelsea

--- resources/views/pages/chelsea.blade.php
+++ resources/views/pages/chelsea.blade.php
@@ -10,10 +10,20 @@ ccmakesthings
     <div class="row">
         <div class="col-md-12">
 
-                <div id="cards" class="row">
+                <div class="row">
                     <?php $x=1; ?>
                     @foreach($volumes as $volume)
-                        {{$volume->title_j}} {{$volume->title_e	}}<br>
+                    	<div class="col-md-4">
+                    		<div class="row">
+                    			<div class="col-md-8">
+                    				<img class="img-fluid" src="/images/chelsea/doujin/{{$volume->id}}.jpg">
+                    			</div>
+                    			<div class="col-md-4">
+                    				{{$volume->title_j}} {{$volume->title_e	}}<br>
+                    			</div>
+                    		</div>
+                        
+                        </div>
                         <?php
                             if ($x%4==0) {
 ?>
@@ -30,17 +40,6 @@ ccmakesthings
         </div>
     </div>
 		
-		@foreach($volumes as $volume)
-		<div class="row">
-			<div class="col-md-4">
-				<img class="img-fluid" src="/images/chelsea/doujin/{{$volume->id}}.jpg">
-			</div>
-			<div class="col-md-8">
-				{{$volume->title_j}} {{$volume->title_e	}}<br>
-			</div>
-			</div>
-			 
-		@endforeach
 		
 	</div>
 
