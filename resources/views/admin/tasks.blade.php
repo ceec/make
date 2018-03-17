f3cc9ca88a189cbb1b6350b2165b5574dc1f6cd8
Christine Stone <cc@battab.com>
2018-03-17 11:57:59 -0600

    test name error - remove name

--- resources/views/admin/tasks.blade.php
+++ resources/views/admin/tasks.blade.php
@@ -7,16 +7,11 @@
 <div class="container flex">  
             @foreach($projects as $project)
                 <div>
+                    <h2>{{$project->project->name}}</h2>
+                    @foreach($project->tasks as $task)
+                        {{$task->task}}<small>{{$task->created_at}}</small><br>
+                    @endforeach
 
-
-                    {!! Form::open(['url' => '/add/task']) !!}
-
-                        {!! Form::text('task','',['class'=>'','id'=>'content']) !!}
-                        {!! Form::hidden('project_id',$project->project_id) !!} 
-
-                        {!! Form::submit('Add','',['class'=>'btn btn-default']) !!}
-
-                    {!! Form::close() !!}        
                  </div>             
       @endforeach
     
