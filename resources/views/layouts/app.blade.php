2eb3b3cc1f4c73b99848f1cdc6e4317c7aa26259
Christine Stone <cc@battab.com>
2018-06-12 20:42:40 -0600

    add dashboard link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,6 +88,7 @@
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
                         @else
+                            <li><a href="/home">Dashboard</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
