f361a48e9b0bb6445b068d5c9bc6c54bacee5bfb
Christine Stone <cc@battab.com>
2018-07-17 19:26:23 -0600

    add url formatter

--- app/Category.php
+++ app/Category.php
@@ -9,4 +9,21 @@ class Category extends Model
      protected $table = 'categories';
 
     
+     /**
+     * generate a friendly url slug
+     */
+    public function url()
+    {
+        $name = $this->name;
+
+        //all lowercase
+        $name = strtolower($name);
+
+        //replace spaces with dashes
+        $url = str_replace(' ','-',$name);
+        
+        return $url;
+    } 
+
+
 }
