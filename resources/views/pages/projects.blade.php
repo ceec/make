45c8e0a0c3051346b9b66c3c636193888ee5301a
Christine Stone <cc@battab.com>
2018-10-07 22:46:22 -0600

    move subquery to eloquent

--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -28,15 +28,16 @@ Projects
 		<h2>Projects</h2>
 			<div class="row">
 			<?php $x=1; ?>
+		
 			@foreach ($projects as $project)
 				<div class="col-sm-3">
 					<div class="card">
 						<div class="card-body">
 							<h5 class="card-title"><a href="/project/{{$project->url}}">{{$project->name}}</a></h5>
-							@if (count($project->recentStep) > 0)
-								<p class="card-text">{!! $project->recentStep[0]->name !!}</p>
-								<span>{{$project->recentStep[0]->updated_at}}</span>
-							@endif
+							
+						<p class="card-text">{{$project->recentStep[0]->name}}</p>
+						<span>{{date('F j, Y g:i a',strtotime($project->lastUpdated))}}</span>
+				
 						</div>
 					</div>
 				</div>			
@@ -60,3 +61,22 @@ Projects
 	</div>
 
 @endsection
+
+
+	<!--		<ul>
+			<li><a href="/movies">Movies Seen</a></li>
+			<li><a href="/london">London Trip</a></li>
+			<li><a href="/chelsea">Chelsea Doujin</a></li>
+			<li><a href="/manga">Manga Collected</a></li>
+			<li><a href="/wordcount">Wordcount</a></li>
+			<li><a href="/books">Books</a></li>		
+			<li><a href="/caterpillars">Caterpillars</a></li>			
+		</ul>-->
+
+<!--		<h2>To move Over</h2>
+		<ul>
+			<li>Doujin</li>
+			<li>Books</li>
+			<li>Rocks</li>	
+			<li>Trips</li>
+		</ul>-->
