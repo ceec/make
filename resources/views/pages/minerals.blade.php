5ae2d7078a0c573d1a77ee60b79baf5673720276
Christine Stone <cc@battab.com>
2019-05-11 11:12:44 -0600

    Switch panels to bootstrap4 cards

--- resources/views/pages/minerals.blade.php
+++ resources/views/pages/minerals.blade.php
@@ -10,18 +10,17 @@ Rocks
 @section('content')
 <div class="container">
     <div class="row">
-    @foreach (array_chunk($minerals->getCollection()->all(),3) as $row)        
+    @foreach (array_chunk($minerals->getCollection()->all(),3) as $row)   
         <div class="row">
-            @foreach ($row as $key => $mineral)
+            @foreach ($row as $key => $mineral)         
             <div class="col-md-4">
-                <div class="panel panel-primary">
-                <div class="panel-heading">
-                  <h3 class="panel-title">{{$mineral->name}} <small class="pull-right">{!! $mineral->formula !!}</small></h3>
-                </div>
-                <div class="panel-body">
+              <div class="card" style="width: 18rem;">
                 <a href="rocks/{{$mineral->name}}"><img class="img-responsive" src="/images/rocks/{{$mineral->image[0]}}"></a>
+                <div class="card-body">
+                  <h5 class="card-title">{{$mineral->name}} <small class="pull-right">{!! $mineral->formula !!}</small></h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
-              </div>
+              </div>            
             </div>
             @endforeach
         </div>            
