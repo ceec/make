7c858d9bfd0d4c4a0814d82a7577fc4af6a1705d
Christine Stone <cc@battab.com>
2019-09-12 23:44:11 -0400

    Add catch for bad sensor data

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -33,6 +33,18 @@ class WeatherController extends Controller
         // Need to get data for last week
         $data = Weather::where('created_at', '>=', Carbon::now()->subWeek())->get();
 
+        // Check if there were any errors
+        $count = Weather::where('temperature','=','-143.68')->where('created_at', '>=', Carbon::now()->subWeek())->count();
+        if ($count > 0) {
+          // Loop through the errors and set -143.68 to ''
+          //$data = json_encode($data);
+          foreach($data as $key => $point) {
+            if ($point->temperature == '-143.68') {
+              $point->temperature = '';
+            }
+          }
+        }
+
         echo json_encode($data);
     }
 
