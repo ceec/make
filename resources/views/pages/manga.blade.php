60ad03c0b13a37c5f6aa2d4e1b1ff0017dc70a38
Christine Stone <cc@battab.com>
2019-12-30 00:13:05 -0700

    Switch manga display to use flexbox

--- resources/views/pages/manga.blade.php
+++ resources/views/pages/manga.blade.php
@@ -3,95 +3,61 @@
 
 
 @section('content')
+
+<style>
+    .manga-container {
+        display: flex;
+        flex-wrap: wrap;
+    }
+.mangacheck {
+    border: 2px solid darkgreen;
+    margin: 5px;
+    padding: 0px 15px;
+}
+
+.manga-have {
+    background-color: green;
+    color:lightgreen;
+}
+
+
+</style>
+
+
 <div class="container">
         <h2>Manga</h2>
+        
         @foreach($mangas as $manga)
             {{$manga->title}}<br>
-                
+            <div class="manga-container">
             @foreach ($manga->volumes()->get() as $volume)
-                <canvas  width="40" height="40" id="myCanvas-{{$volume->id}}"></canvas>
-
-                @if ($volume->have == 1)
-                    <script>
-                    var have = 1;
-                    </script>
-                @else
-                    <script>
-                    var have = 0;
-                    </script>
-                @endif
-
-                <script>
-                    var volume = <?php print $volume->volume; ?>
-                </script>
-
-                <script>
-                    var c=document.getElementById("myCanvas-<?php print $volume->id; ?>");
-                    var ctx=c.getContext("2d");
-
-                    if (have == 1) {
-                        ctx.fillStyle = 'green';
-                    } else {
-                       ctx.fillStyle = 'white';
-                    }
-
-                    
-                     ctx.fillRect(20,20,20,20);
-                     ctx.strokeRect(20,20,20,20);
-                     ctx.stroke();
-
-                    ctx.fillStyle = "black";
-                    ctx.font = "10px serif";
-                    ctx.fillText(volume,24,15);                
-                </script>
-
-                
+                <div class="mangacheck 
+                    @if ($volume->have == 1)
+                    manga-have
+                    @endif
+                ">
+                {{$volume->volume}}<br>
+                </div>
             @endforeach
-            <br>
+            </div>
         @endforeach
+
         <h2>Art Books</h2>
+
         @foreach($artbooks as $artbook)
             {{$artbook->title}}<br>
+            <div class="manga-container">
             @foreach ($artbook->volumes()->get() as $volume)
-                <canvas  width="40" height="40" id="myCanvas-{{$volume->id}}"></canvas>
-
-                @if ($volume->have == 1)
-                    <script>
-                    var have = 1;
-                    </script>
-                @else
-                    <script>
-                    var have = 0;
-                    </script>
-                @endif
-
-                <script>
-                    var volume = <?php print $volume->volume; ?>
-                </script>
-
-                <script>
-                    var c=document.getElementById("myCanvas-<?php print $volume->id; ?>");
-                    var ctx=c.getContext("2d");
-
-                    if (have == 1) {
-                        ctx.fillStyle = 'green';
-                    } else {
-                       ctx.fillStyle = 'white';
-                    }
-
-                    
-                     ctx.fillRect(20,20,20,20);
-                     ctx.strokeRect(20,20,20,20);
-                     ctx.stroke();
-
-                    ctx.fillStyle = "black";
-                    ctx.font = "10px serif";
-                    ctx.fillText(volume,24,15);                
-                </script>
-
-                
-            @endforeach    
-            <br>        
-        @endforeach        
+                <div class="mangacheck 
+                    @if ($volume->have == 1)
+                    manga-have
+                    @endif
+                ">
+                {{$volume->volume}}<br>
+                </div>
+            @endforeach
+            </div>
+        @endforeach
+  
 </div>
 @endsection
