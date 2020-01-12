2106959bd2da9f8d0191f80eb8ff9f34454720a1
Christine Stone <cc@battab.com>
2020-01-12 00:46:38 -0700

    Fix image to link to volume not group

--- resources/views/pages/doujinGroup.blade.php
+++ resources/views/pages/doujinGroup.blade.php
@@ -13,7 +13,7 @@ ccmakesthings
       @foreach($volumes as $volume)
         <div class="col-md-2">
           <div class="card">
-            <img class="card-img-top" src="/images/doujin/{{$group->url}}.jpg" alt="Card image cap">
+            <img class="card-img-top" src="/images/doujin/{{$volume->id}}.jpg" alt="Card image cap">
             <div class="card-block">
             <h4 class="card-title"><a href="/doujin/{{$group->url}}/{{$volume->id}}">{{$volume->title_e}}</a></h4>
             </div>
