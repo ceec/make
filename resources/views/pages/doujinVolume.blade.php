6b8c8a48480861a5e5609c9e5f01817b27c24503
Christine Stone <cc@battab.com>
2020-01-16 23:43:34 -0700

    Fix back image

--- resources/views/pages/doujinVolume.blade.php
+++ resources/views/pages/doujinVolume.blade.php
@@ -13,9 +13,11 @@ ccmakesthings
     <div class="row">
       <div class="col-md-4">
         <img src="/images/doujin/{{$volume->id}}.jpg">
-        <img src="/images/doujin/{{$volume->id}}b.jpg">
       </div>
-      <div class="col-md-8">
+      <div class="col-md-4">
+        <img src="/images/doujin/{{$volume->id}}b.jpg">
+      </div>      
+      <div class="col-md-4">
         <h3>{{$volume->title_e}}</h3>
         <h6>{{$volume->title_j}}</h6>
         <h6>Published: {{$volume->published_date}}</h6>
